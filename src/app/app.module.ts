1319945568090262d5cd16a401f41427f5f65787
Miquel Puig <mpuig22@gmail.com>
2019-02-06 17:06:53 +0100

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { NgModule } from '@angular/core';
 import {APP_ROUTING} from './app.routes';
 
 // Servicios
-
+import {HeroesService} from './services/heroes.service';
 // Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import {HeroeComponent} from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +20,16 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
